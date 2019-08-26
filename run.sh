f730a4d3b78c0c4bf059590673ce33ed6a5b8b7e
Will Tackett <will.s.tackett@gmail.com>
2019-08-26 13:55:30 -0400

    update run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,37 @@
 #!/bin/bash
+#
+#
+#
 
 FLYWHEEL_BASE=/flywheel/v0
-CONTAINER='[flywheel/presurgical-report]'
+MANIFEST_FILE=${FLYWHEEL_BASE}/manifest.json
+INPUT_DIR=${FLYWHEEL_BASE}/input
+mkdir -p ${INPUT_DIR}
+OUTPUT_DIR=${FLYWHEEL_BASE}/output
+mkdir -p ${OUTPUT_DIR}
+BIDS_DIR=${INPUT_DIR}/bids_dataset
+CONTAINER='[flywheel/presurgicalreport]'
+cp ${FLYWHEEL_BASE}/fmriprep_dir ${INPUT_DIR}/bids_dataset/derivatives/fmriprep/fmriprep_dir
+    
 
-#bash fw-heudiconv-export --project my_project --path flywheel/v0/
+# CREATE A BIDS FORMATTED DIRECTORY
+#   Use fw-heudiconv to accomplish this task
+/usr/local/miniconda/bin/python ${FLYWHEEL_BASE}/create_archive_fw_heudiconv.py
+if [[ $? != 0 ]]; then
+  echo "$CONTAINER  Problem creating archive! Exiting (1)"
+  exit 1
+fi
+
+# VALIDATE INPUT DATA
+# Check if the input directory is not empty
+if [[ "$(ls -A $INPUT_DIR)" ]] ; then
+    echo "$CONTAINER  Starting..."
+else
+    echo "Input directory is empty: $INPUT_DIR"
+    exit 1
+fi
+
+# Show the contents of the BIDS directory
+ls -R ${BIDS_DIR}
 
 python report.py /flywheel/v0/bids_dataset /flywheel/v0/bids_dataset/derivatives/fmriprep/ /flywheel/v0/outputs
