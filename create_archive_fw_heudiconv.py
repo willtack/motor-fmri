a95efe5d84c5b397ff143235085f55ed9b9bb4ea
Will Tackett <will.s.tackett@gmail.com>
2019-08-26 10:47:40 -0400

    Add bids exportation script

---
+++ create_archive_fw_heudiconv.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+import json
+import flywheel
+import os
+import shutil
+import logging
+from fw_heudiconv.cli import curate, export, tabulate
+from fw_heudiconv.query import print_directory_tree
+
+
+# logging stuff
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger('fw-heudiconv-gear')
+logger.info("=======: fw-heudiconv starting up :=======")
+
+# start up inputs
+invocation = json.loads(open('config.json').read())
+config = invocation['config']
+inputs = invocation['inputs']
+destination = invocation['destination']
+
+fw = flywheel.Flywheel(inputs['api_key']['key'])
+user = fw.get_current_user()
+
+# start up logic:
+heuristic = None  # inputs['heuristic']['location']['path']
+analysis_container = fw.get(destination['id'])
+project_container = fw.get(analysis_container.parents['project'])
+project_label = project_container.label
+dry_run = False  # config['dry_run']
+action = "Export"  # config['action']
+
+# whole project, single session?
+do_whole_project = False  # config['do_whole_project']
+
+if not do_whole_project:
+
+    # find session object origin
+    session_container = fw.get(analysis_container.parent['id'])
+    sessions = [session_container.label]
+    # find subject object origin
+    subject_container = fw.get(session_container.parents['subject'])
+    subjects = [subject_container.label]
+
+else:
+    sessions = None
+    subjects = None
+
+# logging stuff
+logger.info("Running fw-heudiconv with the following settings:")
+logger.info("Project: {}".format(project_label))
+logger.info("Subject(s): {}".format(subjects))
+logger.info("Session(s): {}".format(sessions))
+logger.info("Action: {}".format(action))
+logger.info("Dry run: {}".format(dry_run))
+
+# action
+if action == "Curate":
+    print("This gear does not do BIDS Curation!")
+
+elif action == "Export":
+
+    downloads = export.gather_bids(fw, project_label, subjects, sessions)
+    export.download_bids(fw, downloads, "/flywheel/v0/input", dry_run=dry_run)
+    if not dry_run:
+        pass
+
+elif action == "Tabulate":
+    print("This gear does not do BIDS Tabulation!")
+else:
+    raise Exception('Action not specified correctly!')
